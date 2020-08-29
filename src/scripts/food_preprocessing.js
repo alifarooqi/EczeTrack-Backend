f5474ccb24d968ec0fb7babb1c1ee652a2362878
Ali Farooqi <m.ali_farooqi@hotmail.com>
2020-08-29 21:15:46 +0800

    Added food preprocessor

---
+++ src/scripts/food_preprocessing.js
@@ -0,0 +1,38 @@
+const fooddata = require('./fooddata');
+const fs = require('fs');
+
+const preprocess = async () => {
+  let output = {};
+  console.log(fooddata.length);
+
+  for(let food of fooddata){
+    const desc = food['Main food description'];
+    const foodclass = food['Food Class'];
+    const quantity = parseInt(food['Ingredient weight (g)']);
+
+    if(output[desc]){
+      if(output[desc][foodclass]){
+        output[desc][foodclass] += quantity;
+      }
+      else
+        output[desc][foodclass] = quantity;
+    }
+    else {
+      output[desc] = {};
+      output[desc][foodclass] = quantity;
+    }
+  }
+
+  return output;
+};
+
+
+const main = async ()=>{
+  let output = await preprocess();
+
+  fs.writeFileSync('food_desc_to_class.json', JSON.stringify(output, null, 2));
+
+
+};
+
+main();
