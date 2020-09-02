3ed3b3c16432163ec0df33ffaacac90279629d54
OptimusBlack <emilakhani@gmail.com>
2020-09-03 00:30:16 +0800

    MSU service completed

--- src/services/msu.service.js
+++ src/services/msu.service.js
@@ -1,17 +1,21 @@
 const { getFactorFromRange, formatDay } = require('./common.service');
 
 const getData = async (dateFrom, dateTo, userId) => {
-  let { data: msus, all_rows } = await getFactorFromRange(dateFrom, dateTo, userId, 'msu');
+  let { data: msus, dates } = await getFactorFromRange(dateFrom, dateTo, userId, 'msu');
 
-  let days = (new Array(all_rows.length)).fill(0);
-  let data = [(new Array(all_rows.length)).fill(0), (new Array(all_rows.length)).fill(0)];
+  let days = (new Array(dates.length)).fill(0);
+  let data = [(new Array(dates.length)).fill(0), (new Array(dates.length)).fill(0)];
   let legend = ['Steroid Use', 'Moisturizer Use'];
 
-  for (let i = 0; i < all_rows.length; i++) {
-    days[i] = formatDay(all_rows[i].createdAt);
-    data[0][i] = msus.steroidUse;
-    data[1][i] = msus.moisturizerUse;
+  for (let i = 0; i < dates.length; i++) {
+    days[i] = formatDay(dates[i]);
+    data[0][i] = msus[i].steroidUse;
+    data[1][i] = msus[i].moisturizerUse;
   }
 
   return {days, data, legend};
 };
+
+module.exports = {
+  getData
+}
