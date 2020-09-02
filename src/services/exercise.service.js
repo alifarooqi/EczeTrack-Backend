bc7e0874c2bea397ace14688020b2348fd0b0f2e
Ali Farooqi <m.ali_farooqi@hotmail.com>
2020-09-03 00:11:14 +0800

    added sleep calculations

--- src/services/exercise.service.js
+++ src/services/exercise.service.js
@@ -1,8 +1,4 @@
-const httpStatus = require('http-status');
-const { Exercise, Weekly } = require('../models');
-const ApiError = require('../utils/ApiError');
-
-const {ObjectId} = require('mongoose').Types;
+const { getFactorFromRange } = require('./common.service');
 
 const calculate = (exercise) =>{
   let score = 0;
@@ -17,26 +13,13 @@ const calculate = (exercise) =>{
 };
 
 const getData = async (dateFrom, dateTo, userId) => {
-  let all_rows = await Weekly.find({
-    userId: ObjectId(userId),
-    createdAt: {
-      $gte: dateFrom,
-      $lt: dateTo
-    }
-  });
+  let {data: exercise, all_rows} = await getFactorFromRange(dateFrom, dateTo, userId, 'exercise');
 
-  let ids = new Array(all_rows.length);
-  for (let i=0; i< all_rows.length; i++){
-    ids[i] = all_rows[i].exercise;
-  }
-
-  let exercise = await Exercise.find().where('_id').in(ids).exec();
-
-  let weeks = (new Array(all_rows.length)).fill(0);
-  let data = (new Array(all_rows.length)).fill(0);
+  let weeks = (new Array(exercise.length)).fill(0);
+  let data = (new Array(exercise.length)).fill(0);
   let legend = ['Exercise'];
 
-  for (let i=0; i< all_rows.length; i++){
+  for (let i=0; i< exercise.length; i++){
     weeks[i] = all_rows[i].week;
     data[i] = calculate(exercise[i]);
   }
--- src/services/sleep.service.js
+++ src/services/sleep.service.js
@@ -1,3 +1,29 @@
-const httpStatus = require('http-status');
-const { Sleep, Weekly } = require('../models');
-const ApiError = require('../utils/ApiError');
+const { getFactorFromRange } = require('./common.service');
+
+const calculate = (sleep) =>{
+  let score = 0;
+  for(let q=0; q<5; q++)
+    score += sleep[q];
+
+  return score;
+};
+
+const getData = async (dateFrom, dateTo, userId) => {
+  let {data: sleep, all_rows} = await getFactorFromRange(dateFrom, dateTo, userId, 'sleep');
+
+  let weeks = (new Array(sleep.length)).fill(0);
+  let data = (new Array(sleep.length)).fill(0);
+  let legend = ['Sleep'];
+
+  for (let i=0; i< sleep.length; i++){
+    weeks[i] = all_rows[i].week;
+    data[i] = calculate(sleep[i]);
+  }
+  data = [data]; // Converting it into 2D Array for the charts.
+
+  return {weeks, data, legend};
+};
+
+module.exports = {
+  getData
+};
