77280097c46edf6e179d83f814d574045dac6e9f
Muhammad Ali Farooqi <m.ali_farooqi@hotmail.com>
2020-09-14 10:32:41 +0800

    Update exercise.service.js

--- src/services/exercise.service.js
+++ src/services/exercise.service.js
@@ -1,4 +1,4 @@
-const { getFactorFromRange } = require('./common.service');
+const { getFactorFromRange, formatDay } = require('./common.service');
 
 const calculate = (exercise) =>{
   let score = 0;
